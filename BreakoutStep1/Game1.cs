066e6fd5fc066a88f4b4dd2aba7fbee2844f377a
MichaelBreedloveColumbia <112978110+MichaelBreedloveColumbia@users.noreply.github.com>
2022-11-01 02:24:48 -0500

    Add files via upload

---
+++ BreakoutStep1/Game1.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary.Util;
+using System.Collections.Generic;
+
+namespace BreakoutStep1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+
+        Ball ball;
+        BlockManager manager;
+
+        Paddle paddle;
+        InputHandler input;
+
+        int score = 0;
+        int lives = 3;
+
+        bool Active = false;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            input = (InputHandler)this.Services.GetService(typeof(IInputHandler));
+
+            NewGame(true, false);
+        }
+
+        void NewGame(bool newPaddle = false, bool overridePaddleBall = true)
+        {
+            manager = new BlockManager(this);
+            GenerateLayout();
+
+            SpawnBall(overridePaddleBall);
+
+            if (newPaddle)
+            {
+                paddle = new Paddle(this, ball);
+                this.Components.Add(paddle);
+            }
+
+            score = 0;
+            lives = 3;
+
+            Active = true;
+        }
+
+        void EndGame()
+        {
+            Active = false;
+            foreach(MonogameBlock block in manager.Blocks)
+            {
+                this.Components.Remove(block);
+            }
+
+            manager.RemoveAll();
+            this.Components.Remove(ball);
+        }
+
+        void GenerateLayout()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    MonogameBlock block = new MonogameBlock(this);
+                    block.Location = new Vector2(40 + (10 * j) + i * 60, 50 + 50 * j);
+                    this.Components.Add(block);
+                    manager.AddBlock(block);
+                }
+            }
+        }
+
+        void SpawnBall(bool OverridePaddleBall = false)
+        {
+            ball = new Ball(this);
+            this.Components.Add(ball);
+
+            if (OverridePaddleBall)
+            {
+                paddle.ball = ball;
+                paddle.controller.ball = ball;
+            }
+        }
+
+        void AddScore()
+        {
+            score++;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("Arial");
+
+            // TODO: use this.Content to load your game content here
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            foreach(MonogameBlock block in manager.Blocks)
+            {
+                if (block.BlockState == BlockState.Broken)
+                {
+                    AddScore();
+                    this.Components.Remove(block);
+                }
+            }
+            
+            manager.UpdateManager();
+
+            if (ball.State == BallState.MarkedForRemoval)
+            {
+                this.Components.Remove(ball);
+                lives--;
+                SpawnBall(true);
+            }
+
+            ball.UpdateBall(gameTime, manager.Blocks);
+            
+            if (manager.State == ManagerStatus.Inactive && Active || lives < 1)
+            {
+                EndGame();
+            }
+
+            if (!Active && Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                NewGame();
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            if (manager.State == ManagerStatus.Active)
+            {
+                spriteBatch.DrawString(font, $"Score: {score}        Lives: {lives}        Current Ball Speed: {ball.Speed}", new Vector2(0, 0), Color.White);
+            }
+            else
+            {
+                if (lives < 1)
+                {
+                    spriteBatch.DrawString(font, "Game over! Press space to play again.", new Vector2(0, 0), Color.White);
+                }
+                else
+                {
+                    spriteBatch.DrawString(font, "You won! Press space to play again.", new Vector2(0, 0), Color.White);
+                }
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ BreakoutStep1/Paddle.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameLibrary.Sprite;
+using MonoGameLibrary.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakoutStep1
+{
+    class Paddle : DrawableSprite
+    {
+        int BallCollisionCD = 0;
+
+        //Service Dependencies
+        GameConsole console;
+
+        //Dependencies
+        public PaddleController controller;
+        public Ball ball;      //Need reference to ball for collision
+
+        public Paddle(Game game, Ball b)
+            : base(game)
+        {
+            this.Speed = 800;
+            this.ball = b;
+            controller = new PaddleController(game, ball);
+
+            //Lazy load GameConsole
+            console = (GameConsole)this.Game.Services.GetService(typeof(IGameConsole));
+            if (console == null) //ohh no no console make a new one and add it to the game
+            {
+                console = new GameConsole(this.Game);
+                this.Game.Components.Add(console);  //add a new game console to Game
+            }
+        }
+
+        protected override void LoadContent()
+        {
+            this.spriteTexture = this.Game.Content.Load<Texture2D>("paddleSmall");
+            SetInitialLocation();
+            base.LoadContent();
+        }
+
+        public void SetInitialLocation()
+        {
+            this.Location = new Vector2(GraphicsDevice.Viewport.Width * 0.5f, GraphicsDevice.Viewport.Height * 0.95f);
+        }
+
+        Rectangle collisionRectangle;  //Rectangle for paddle collision uses just the top of the paddle instead of the whole sprite
+
+        public override void Update(GameTime gameTime)
+        {
+            if (BallCollisionCD > 0)
+            {
+                BallCollisionCD--;
+            }
+
+            //Update Collision Rect
+            collisionRectangle = new Rectangle((int)this.Location.X, (int)this.Location.Y, this.spriteTexture.Width, 1);
+
+            //Deal with ball state
+            switch (ball.State)
+            {
+                case BallState.OnPaddleStart:
+                    //Move the ball with the paddle until launch
+                    UpdateMoveBallWithPaddle();
+                    break;
+                case BallState.Playing:
+                    UpdateCheckBallCollision();
+                    break;
+            }
+
+            //Movement from controller
+            controller.HandleInput(gameTime);
+
+            this.Direction = controller.Direction;
+            this.Location += this.Direction * (this.Speed * gameTime.ElapsedGameTime.Milliseconds / 1000);
+
+            KeepPaddleOnScreen();
+            base.Update(gameTime);
+        }
+
+        private void UpdateMoveBallWithPaddle()
+        {
+            ball.Speed = 0;
+            ball.Direction = Vector2.Zero;
+            ball.Location = new Vector2(this.Location.X + (this.LocationRect.Width / 2 - ball.SpriteTexture.Width / 2), this.Location.Y - ball.SpriteTexture.Height);
+        }
+
+        private void UpdateCheckBallCollision()
+        {
+            if (Intersects(ball) && ball.Location.Y + ball.spriteTexture.Height <= Location.Y + spriteTexture.Height && BallCollisionCD < 1)
+            {
+                ball.Direction.Y *= -1;
+                BallCollisionCD = 4;
+                ball.MultiplySpeed(1.2f);
+
+                /*if (ball.Location.X + ball.SpriteTexture.Width * 0.5f <= Location.X - spriteTexture.Width * 0.5f)
+                {
+                    ball.Direction.X 
+                }*/
+            }
+        }
+
+        private void KeepPaddleOnScreen()
+        {
+            this.Location.X = MathHelper.Clamp(this.Location.X, 0, this.Game.GraphicsDevice.Viewport.Width - this.spriteTexture.Width);
+        }
+    }
+}
